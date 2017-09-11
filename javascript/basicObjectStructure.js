2c0c72e282bed81743e1a74ff9981b1911a6ec68
Jeff Mattson <jeff@wedu.com>
2017-09-11 11:39:00 -0400

    Basic javascipt object.

---
+++ javascript/basicObjectStructure.js
@@ -0,0 +1,17 @@
+/* This is the structure of a basic javascript 'object' */
+$(function(){
+    object_js.init();
+});
+
+var object_js = {
+
+    variables : '',
+
+    init: function(){
+        this.awesomeFunction(;)
+    },
+
+    awesomeFunction : function(){
+        console.log('Stuff happening here for sure.');
+    }
+}
